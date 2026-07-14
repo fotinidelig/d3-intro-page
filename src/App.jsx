113353ebda58f30dc7232b8b0cbea0b5ea2a0dfd
fotini delig (mac) <fotinidelig@gmail.com>
2026-07-14 19:29:42 +0300

    added profile picture to assets

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <main className="app-main">
         <aside className="app-profile" aria-label="About">
           <div className="app-avatar" role="img" aria-label="Profile picture placeholder" >
-            <img src="/profil_pic.jpg" alt="Profile picture" />
+            <img src="src/assets/profil_pic.jpg" alt="Profile picture" />
             </div>
           <h1 className="app-title">Hi, it's Fotini.</h1>
           <p className="app-bio" style={{ textAlign: 'center', color: 'black' }}>
